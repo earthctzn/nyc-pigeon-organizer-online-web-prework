393a72385de039b843f635a28d0702d0a3362766
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 16:40:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  
+  data.each_with_object({}) do |(key, x), result|
+    x.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
 end
